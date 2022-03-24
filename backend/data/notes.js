281a4e847fc4a26f76cf25e9229b0e0f97488ed0
Leonardo Bravo P <leo.bravoparedes@gmail.com>
2022-03-24 11:48:39 -0300

    Initialized our Backend and Frontend

---
+++ backend/data/notes.js
@@ -0,0 +1,16 @@
+const notes = [
+    {
+        _id: "1",
+        title: "Day 1 of college",
+        content: "I made a few new friends",
+        category: "College",
+    },
+    {
+        _id: "2",
+        title: "Learned some Node JS",
+        content: "Learned how to create a server",
+        category: "Learning",
+    }
+]
+
+module.exports = notes
